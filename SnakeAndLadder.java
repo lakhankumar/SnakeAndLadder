5648b6d65b5fe17035d6053eee3f180f4f1a2105
lakhankumar <lakhankumar997@gmail.com>
2021-08-23 19:46:52 +0530

    Rolling a die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,13 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game!!!");
-        int initialPosition= 0;
-        System.out.println("The initial position of a player is : "+initialPosition);
+		System.out.println("Welcome to Snake and Ladder Game");
+        int startPosition= 0;
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 6);
+        System.out.println("The initial position of a player is : " +startPosition);
+        System.out.println("After rolling a die position of player is at : " +randomNum);
 	}
 
 }
