3fbee4bf873aee1dc2c23065f3aaab7dda953a8e
lakhankumar <lakhankumar997@gmail.com>
2021-08-23 19:41:28 +0530

    welcome to snake and ladder game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Game!!!");
+        int initialPosition= 0;
+        System.out.println("The initial position of a player is : "+initialPosition);
+	}
+
+}
