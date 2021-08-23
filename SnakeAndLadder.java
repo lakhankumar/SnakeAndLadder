f91b3afaa893ec837d7c966d0753a74f340f39bb
lakhankumar <lakhankumar997@gmail.com>
2021-08-23 20:02:04 +0530

    until player reachers to winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,27 +4,28 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 			System.out.println("Welcome to Snake and Ladder Game!!!");
-	        int START_POSITION= 0;
+	        int startPosition= 0;
 	        int currentPosition = 0;
-	        int NO_PLAY = 0;
-	        int LADDER = 1;
-	        int SNAKE = 2;
-	        System.out.println("The initial position of a player is : "+START_POSITION);
+	        int noPlay = 0;
+	        int ladder = 1;
+	        int snake = 2;
+	        int winningPosition=100;
+	        System.out.println("The initial position of a player is : "+startPosition);
 	        
-	        for(int i =1;i<=10;i++){
+	        while(currentPosition<=winningPosition){
 	            int diceNumber = ThreadLocalRandom.current().nextInt(1,6);
 	            int nextMove = ThreadLocalRandom.current().nextInt(0, 2);
 	            System.out.println("Random dice number generated is : "+diceNumber);
 	            
-	            if(nextMove == NO_PLAY){
+	            if(nextMove == noPlay){
 	                System.out.println("Sorry you can not Play,You are at the Same Position");
 	                
 	            }
-	            else if(nextMove == LADDER){
+	            else if(nextMove == ladder){
 	                System.out.println("You have got the ladder");
 	                currentPosition = currentPosition + diceNumber;
 	            }
-	            else if (nextMove ==  SNAKE){
+	            else if (nextMove ==  snake){
 	                System.out.println("ITS a SNAKE");
 	                
 	                if(currentPosition != 0) {
